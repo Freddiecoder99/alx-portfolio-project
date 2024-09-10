7db33e3be2cd1e5e17e38df54878e12372f73d3d
Freddiecoder99 <rickyfredo77@gmail.com>
2024-09-11 02:53:54 +0300

    website

---
+++ script.js
@@ -0,0 +1,32 @@
+// Description: This file contains the JavaScript code for the terms and privacy modals.
+var termsModal = document.getElementById("terms-modal");
+var privacyModal = document.getElementById("privacy-modal");
+
+var termsLink = document.getElementById("terms-link");
+var privacyLink = document.getElementById("privacy-link");
+
+var closeTerms = document.getElementById("close-terms");
+var closePrivacy = document.getElementById("close-privacy");
+
+termsLink.onclick = function() {
+    termsModal.style.display = "block";
+}
+privacyLink.onclick = function() {
+    privacyModal.style.display = "block";
+}
+
+closeTerms.onclick = function() {
+    termsModal.style.display = "none";
+}
+closePrivacy.onclick = function() {
+    privacyModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == termsModal) {
+        termsModal.style.display = "none";
+    }
+    if (event.target == privacyModal) {
+        privacyModal.style.display = "none";
+    }
+}
